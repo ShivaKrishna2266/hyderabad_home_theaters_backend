a3c6bb21653f6a39d5a9cc17bebdb65808b8bb10
ShivaKrishna2266 <gadudulashiva@gmail.com>
2025-02-18 18:57:21 +0530

    Initial commit

---
+++ src/main/java/com/hyderabad_home_theaters/HyderabadHomeTheatersApplication.java
@@ -0,0 +1,13 @@
+package com.hyderabad_home_theaters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyderabadHomeTheatersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyderabadHomeTheatersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyderabad_home_theaters/HyderabadHomeTheatersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyderabad_home_theaters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyderabadHomeTheatersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
